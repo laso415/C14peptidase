16064e3b17ee5879b54da8977d45324051049e2f
Sori La <61224580+laso415@users.noreply.github.com>
2021-07-28 14:56:15 +0200

    Update C14.sh

--- C14.sh
+++ C14.sh
@@ -5,14 +5,12 @@ hmmsearch --domtblout C14_hmmsearch.txt -E0.00001 Peptidase_C14.hmm C14.fasta
 # Get ID of sequences with E<=0.00001
 awk '{print $1}' C14_hmmsearch.txt > C14_hmmsearch_ID
 
-# filter 989.fasta to only possess sequences with E<=0.00001
+# filter 989.fasta to only possess sequences with E<=0.00001 [The extract_select_seqs.py script can be found at https://github.com/PiscatorX/barcode-rapidus/blob/master/extract_select_seqs.py ]
 ./extract_select_seqs.py -l C14_hmmsearch_ID C14.fasta > C14_hmmsearch.fasta
 
 #Align sequences to Peptidase_C14 HMM profile
 hmmalign --trim Peptidase_C14.hmm C14_filtered.fasta > C14_filtered.sto
 
-# open jalview and manual inspection of HC dyad
-jalview
+#Alignment is used to visually inspect the alignment and save the final alignment
 
-# save the alignment as fasta format C14_filtered_HC.fasta
 
